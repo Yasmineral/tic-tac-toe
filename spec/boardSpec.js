2fb603b0bef9407eafd996e0291f4dab43245f90
yasmine.morawej@gmail.com <yasmine.morawej@gmail.com>
2020-01-24 11:57:21 +0000

    add functionality to switch players after every move

--- spec/boardSpec.js
+++ spec/boardSpec.js
@@ -1,8 +1,15 @@
 describe("Board", function() {
-  it("a claimed square is added to the players list of moves", function() {
-    let board = new Board()
+  beforeEach(function() {
+    board = new Board()
+  })
+  it("the first move is added to player X's list of moves", function() {
     board.claimSquare(1)
     expect(board.playerXMoves).toEqual([1])
+  })
+  it("the second move is added to player O's list of moves", function() {
+    board.claimSquare(1)
+    board.claimSquare(4)
+    expect(board.playerOMoves).toEqual([4])
   })
 })
 
--- src/board.js
+++ src/board.js
@@ -1,9 +1,25 @@
 class Board {
   constructor() {
     this.playerXMoves = []
+    this.playerOMoves = []
+    this.currentPlayer = 'X'
   }
 
   claimSquare(squareNumber) {
-    this.playerXMoves.push(squareNumber)
+    if (this.currentPlayer === 'X') {
+      this.playerXMoves.push(squareNumber)
+    } else {
+      this.playerOMoves.push(squareNumber)
+    }
+    this.switchPlayers()  
+  }
+
+  switchPlayers() {
+    if (this.currentPlayer === 'X') {
+      this.currentPlayer = 'O'
+    } else {
+      this.currentPlayer = 'X'
+    }
   }
 }
+
