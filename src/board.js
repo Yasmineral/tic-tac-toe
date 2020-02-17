20f5b87b560fd52cd322ebe05ba42aa2df5aa1c9
yasmine.morawej@gmail.com <yasmine.morawej@gmail.com>
2020-02-17 17:40:09 +0000

    add winning moves array

--- src/board.js
+++ src/board.js
@@ -1,18 +1,27 @@
 class Board {
   constructor() {
     this.player = "X"
-    this.moves = 
-    [0, 0, 0,
-     0, 0, 0,
-     0, 0, 0 ]
+    this.moves = [
+      0, 0, 0,
+      0, 0, 0,
+      0, 0, 0 
+    ]
+    this.winningMoves = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ]
   }
 
   claimSquare(square) {
     this.moves[square] = (this.player)
     this.switchPlayers()
-  }
-
-
+  };
 
   switchPlayers() {
     if (this.player === "X") {
@@ -20,9 +29,6 @@ class Board {
     } else if (this.player === "O") {
       return this.player = "X"
     }
-  }
-
-  
-}
+  };
 
 
