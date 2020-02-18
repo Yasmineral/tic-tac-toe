1e103b66f7f2b690113ce15aba345bbe819c6c06
yasmine.morawej@gmail.com <yasmine.morawej@gmail.com>
2020-02-18 12:31:13 +0000

    check that all horizontal winning moves return true

--- spec/boardSpec.js
+++ spec/boardSpec.js
@@ -10,22 +10,34 @@ describe("Board", function() {
       board.claimSquare(0)
       expect(board.player).toEqual("O")
     })
-  })
+  });
   describe("#claimSquare", () => {
     it("marks the current player's move within the game", () => {
       board.claimSquare(0)
       expect(board.moves[0]).toEqual("X")
     })
-  })
+  });
   describe("#checkForWinner", () => {
     it("returns false if there are no winning moves", () => {
       board.moves = [0, 0, 0, 0, 0, 0, 0, 0, 0 ]
       expect(board.checkForWinner()).toEqual(false)
     })
-    it("returns true if there is a horizontal winning move", () => {
+    it("returns true if there is a top horizontal winning move", () => {
       board.moves = ["X", "X", "X", 0, 0, 0, 0, 0, 0 ]
       expect(board.checkForWinner()).toEqual(true)
     })
-  })
+    it("returns true if there is a middle horizontal winning move", () => {
+      board.moves = [0, 0, 0, "X", "X", "X", 0, 0, 0 ]
+      expect(board.checkForWinner()).toEqual(true)
+    })
+    it("returns true if there is a bottom horizontal winning move", () => {
+      board.moves = [0, 0, 0, 0, 0, 0, "X", "X", "X" ]
+      expect(board.checkForWinner()).toEqual(true)
+    })
+    it("returns true if there is a left verticle winning move", () => {
+      board.moves = ["X", 0, 0, "X", 0, 0, "X", 0, 0 ]
+      expect(board.checkForWinner()).toEqual(true)
+    })
+  });
 })
 
